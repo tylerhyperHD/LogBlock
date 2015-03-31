2fb706963556da0bcfde4202ce3fe8b3fb602601
Tyler Wrenn <mcbeat257@gmail.com>
2015-03-31 17:42:39 -0400

    Merged branches and updated to 6.0

--- src/main/java/de/diddiz/LogBlock/listeners/BlockBurnLogging.java
+++ src/main/java/de/diddiz/LogBlock/listeners/BlockBurnLogging.java
@@ -1,9 +1,8 @@
 package de.diddiz.LogBlock.listeners;
 
-import static de.diddiz.LogBlock.config.Config.isLogging;
-import static de.diddiz.util.LoggingUtil.smartLogBlockBreak;
-import static de.diddiz.util.LoggingUtil.smartLogFallables;
-
+import de.diddiz.LogBlock.Actor;
+import de.diddiz.LogBlock.LogBlock;
+import de.diddiz.LogBlock.Logging;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -13,35 +12,33 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import de.diddiz.LogBlock.Actor;
-import de.diddiz.LogBlock.LogBlock;
-import de.diddiz.LogBlock.Logging;
+import static de.diddiz.LogBlock.config.Config.isLogging;
+import static de.diddiz.util.LoggingUtil.smartLogBlockBreak;
+import static de.diddiz.util.LoggingUtil.smartLogFallables;
 
-public class BlockBurnLogging extends LoggingListener
-{
-	public BlockBurnLogging(LogBlock lb) {
-		super(lb);
-	}
+public class BlockBurnLogging extends LoggingListener {
+    public BlockBurnLogging(LogBlock lb) {
+        super(lb);
+    }
 
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onBlockBurn(BlockBurnEvent event) {
-		if (isLogging(event.getBlock().getWorld(), Logging.FIRE)) {
-			smartLogBlockBreak(consumer, new Actor("Fire"), event.getBlock());
-			smartLogFallables(consumer, new Actor("Fire"), event.getBlock());
-		}
-	}
-	
-	
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onExtinguish(PlayerInteractEvent event) {
-		Player player = event.getPlayer();
-		Block block = player.getTargetBlock(null, 5);
-		if(event.getAction().equals(Action.LEFT_CLICK_BLOCK)){
-			if (block.getType().equals(Material.FIRE) && isLogging(player.getWorld(), Logging.FIRE)) {
-				Actor actor = Actor.actorFromEntity(player);
-				smartLogBlockBreak(consumer, actor, block);
-				smartLogFallables(consumer, actor, block);
-			}
-		}
-	}
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockBurn(BlockBurnEvent event) {
+        if (isLogging(event.getBlock().getWorld(), Logging.FIRE)) {
+            smartLogBlockBreak(consumer, new Actor("Fire"), event.getBlock());
+            smartLogFallables(consumer, new Actor("Fire"), event.getBlock());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onExtinguish(PlayerInteractEvent event) {
+        if (event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+            Player player = event.getPlayer();
+            Block block = event.getClickedBlock().getRelative(event.getBlockFace());
+            if (block.getType().equals(Material.FIRE) && isLogging(player.getWorld(), Logging.FIRE)) {
+                Actor actor = Actor.actorFromEntity(player);
+                smartLogBlockBreak(consumer, actor, block);
+                smartLogFallables(consumer, actor, block);
+            }
+        }
+    }
 }
